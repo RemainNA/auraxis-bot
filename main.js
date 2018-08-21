6ed7459d7bb98e540ac1432a139e8675de6b7705
ultimastorm <ultimatestormer@gmail.com>
2018-08-21 11:32:41 -0600

    Change to list all created tables

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const SQLclient = new Client({
 
 SQLclient.connect();
 
-SQLclient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+SQLclient.query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
