e655d42d1bb27eb1e0fd6c8550b68719ec0e0d57
ultimastorm <ultimatestormer@gmail.com>
2020-03-09 09:40:04 -0600

    Remove debug text

--- main.js
+++ main.js
@@ -270,7 +270,6 @@ client.on('message', message => {
 				subscription.subscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -285,7 +284,6 @@ client.on('message', message => {
 				subscription.unsubscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -300,7 +298,6 @@ client.on('message', message => {
 				subscription.subscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2ps4us:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -315,7 +312,6 @@ client.on('message', message => {
 				subscription.unsubscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2ps4us:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -330,7 +326,6 @@ client.on('message', message => {
 				subscription.subscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2ps4eu:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -345,7 +340,6 @@ client.on('message', message => {
 				subscription.unsubscribeActivity(SQLclient, message.channel.id, outfits[x], 'ps2ps4eu:v2')
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -360,7 +354,6 @@ client.on('message', message => {
 				subscription.subscribeAlert(SQLclient, message.channel.id, servers[x])
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
@@ -375,7 +368,6 @@ client.on('message', message => {
 				subscription.unsubscribeAlert(SQLclient, message.channel.id, servers[x])
 					.then(res => message.channel.send(res))
 					.catch(err => {
-						console.log(err);
 						if(typeof(err) == "string"){
 							message.channel.send(err);
 						}
