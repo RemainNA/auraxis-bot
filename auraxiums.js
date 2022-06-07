7ee48a4f118317dd0f77a387a1f39050cd2ea362
RemainNA <35419061+RemainNA@users.noreply.github.com>
2022-06-06 21:08:11 -0600

    Several improvements to Auraxium tracking

--- auraxiums.js
+++ auraxiums.js
@@ -2,35 +2,52 @@
 
 const {censusRequest} = require('./utils.js');
 const Discord = require('discord.js');
+const sanction = require('./static/sanction.json');
+const { localeNumber } = require('./utils.js');
 const i18n = require('i18n');
 
 const getAuraxiumList = async function(cName, platform, locale='en-US'){
     // Calculates the number of Auraxium medals a specified character has
-    let response = await censusRequest(platform, 'character_list', `/character?name.first_lower=${cName}&c:join=characters_achievement^list:1^terms:earned_count=1^outer:0^hide:character_id%27earned_count%27start%27finish%27last_save%27last_save_date%27start_date(achievement^terms:repeatable=0^outer:0^show:name.en%27description.en)`);
+    let response = await censusRequest(platform, 'character_list', `/character?name.first_lower=${cName}&c:join=characters_achievement^list:1^outer:0^hide:character_id%27earned_count%27start%27finish%27last_save%27last_save_date%27start_date(achievement^terms:repeatable=0^outer:0^show:name.en%27description.en)&c:resolve=weapon_stat_by_faction`);
     let medalList = [];
+	let confirmedMedals = [];
+	let possibleMedals = [];
 	if(response.length == 0){
 		throw i18n.__mf({phrase: "{name} not found", locale: locale}, {name: cName});
 	}
     let achievementList = response[0].character_id_join_characters_achievement;
     for(const x of achievementList){
         achievement = x.achievement_id_join_achievement;
-        if(achievement != undefined){
+        if(achievement != undefined && x.finish_date != "1970-01-01 00:00:00.0"){
             if(achievement.description == undefined){
                 if(achievement.name.en.indexOf("Auraxium") > -1){
-                    medalList.push([achievement.name.en, Date.parse(x.finish_date)]);
+                    medalList.push([achievement.name.en.split(":")[0], Date.parse(x.finish_date)]);
+					confirmedMedals.push(achievement.name.en.split(":")[0]);
                 }
             }
             else if(achievement.description.en == "1000 Enemies Killed"){
                 medalList.push([achievement.name.en, Date.parse(x.finish_date)]);
+				confirmedMedals.push(achievement.name.en);
             }
         }
     }
+
+	let statList = response[0].stats?.weapon_stat_by_faction;
+	for(const stat of statList){
+		if(stat.stat_name == "weapon_kills" && stat.item_id != 0){
+			const statValue = Number(stat.value_vs) + Number(stat.value_nc) + Number(stat.value_tr);
+			if(statValue >= 1160 && !confirmedMedals.includes(sanction[stat.item_id]?.name)){
+				possibleMedals.push(`${sanction[stat.item_id].name} (${localeNumber(statValue, locale)})`);
+			}
+		}
+	}
 
 	medalList.sort((a,b) => b[1] - a[1]);  //Chronological sort, newest first
     return {
 		name: response[0].name.first, 
 		faction: response[0].faction_id, 
-		medals: medalList
+		medals: medalList,
+		possibleMedals: possibleMedals
 	};
 }
 
@@ -41,7 +58,7 @@ module.exports = {
 		let resEmbed = new Discord.MessageEmbed();
 		resEmbed.setTitle(i18n.__mf({phrase: "{name} Auraxiums", locale: locale}, {name: medalList.name}));
 		let textList = "";
-		let remaining = medalList.medals.length;
+		let remaining = medalList.medals.length + medalList.possibleMedals.length;
 		if(remaining == 0){
 			throw i18n.__mf({phrase: "{name} has no Auraxium medals", locale: locale}, {name: medalList.name})
 		}
@@ -69,6 +86,31 @@ module.exports = {
 			else{
 				resEmbed.setDescription(textList);
 			}
+
+			textList = "";
+			continued = false;
+
+			for(const medal of medalList.possibleMedals){
+				const currentItem = `${medal}\n`;
+				if(!continued && (textList.length + currentItem.length) > 1024){
+					continued = true;
+					resEmbed.addField(i18n.__({phrase: "Possible medals (kills)", locale: locale}), textList);
+					textList = currentItem;
+				}
+				else if(continued && (textList.length + currentItem.length) > 1024){
+					resEmbed.addField(i18n.__({phrase: "Continued...", locale: locale}), textList);
+					textList = currentItem;
+				}
+				else{
+					textList += currentItem;
+				}
+			}
+			if(continued){
+				resEmbed.addField(i18n.__({phrase: "Continued...", locale: locale}), textList);
+			}
+			else{
+				resEmbed.addField(i18n.__({phrase: "Possible medals (kills)", locale: locale}), textList);
+			}
 		}
 		else{
 			let max = 20;
@@ -86,6 +128,20 @@ module.exports = {
 				max -= 1
 			}
 			resEmbed.setDescription(textList);
+			textList = "";
+
+			if(remaining > 0 && max > 0){
+				for(const medal of medalList.possibleMedals){
+					if(max == 0){
+						textList += i18n.__mf({phrase: "And {num} more", locale: locale}, {num: remaining});
+						break;
+					}
+					textList += `${medal}\n`;
+					remaining -= 1;
+					max -= 1
+				}
+				resEmbed.addField(i18n.__({phrase: "Possible medals (kills)", locale: locale}), textList);
+			}
 		}
 		
 		if (medalList.faction == "1"){ //vs
